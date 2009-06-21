d150fe5784072fbb4506bc8d22315c87158cd107
Pedro Palazon <kusorbox@gmail.com>
2009-06-21 16:13:47 +0200

    Day One: new task form.

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,5 +1,12 @@
 system.use("com.joyent.Sammy");
+system.use("com.joyent.Resource");
+
+var Task = new Resource('task');
 
 GET("/", function() {
   return redirect("/index.html");
+});
+
+GET(/\/tasks\/new\/?$/, function() {
+  return template("form.html");
 });
