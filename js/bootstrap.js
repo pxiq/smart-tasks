69cd8ecdd9a1800dfe8dec5f0461fdbc024d00c6
Pedro Palazon <kusorbox@gmail.com>
2009-06-21 16:16:47 +0200

    Day one: save a new Task (POST).

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -10,3 +10,12 @@ GET("/", function() {
 GET(/\/tasks\/new\/?$/, function() {
   return template("form.html");
 });
+
+POST(/\/tasks\/?$/, function() {
+  this.task = new Task();
+  this.task.notes = this.request.body.notes;
+  this.task.title = this.request.body.title;
+
+	this.task.save();
+  return template("task.html");
+});
