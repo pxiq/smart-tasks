061edacce99de439d17f715187e3e1aeeb1240e5
Pedro P. Candel <kusorbox@gmail.com>
2009-06-26 18:21:00 +0200

    Fixed stupid bug on previous commit :-)

--- web/public/js/complete.js
+++ web/public/js/complete.js
@@ -1,31 +1,32 @@
 $(document).ready( function() {
+	$('input[type=checkbox]').each(function ( aCheckbox ){
+		var theCheckbox = this;
+		var theForm = $(theCheckbox).attr('data-complete');
+	  $( "#" + theForm ).bind('submit', function() {
+	    $.ajax({
+	      type: 'PUT',
+	      url: $(this).attr('action'),
+	      dataType: 'json',
+	      data: $(this).serialize(),
+	      processData : false,
+	      success: function(data, textStatus) {
+	        if (data['ok'] == true) {
+	          // Done!.
+	        } else {
+	          alert( "Oooops!, something failed" );
+	        } 
+	      },
+	      error: function (XMLHttpRequest, textStatus, errorThrown) {
+	        alert("Ooooops!, request failed with status: " + XMLHttpRequest.status + ' ' + XMLHttpRequest.responseText);
+	      }
+	    });
+	    return false;
+	  });
 
-  var theCheckbox = $('input[type=checkbox]');
-  var theForm = $(theCheckbox).attr('data-complete');
-  $( "#" + theForm ).bind('submit', function() {
-    $.ajax({
-      type: 'PUT',
-      url: $(this).attr('action'),
-      dataType: 'json',
-      data: $(this).serialize(),
-      processData : false,
-      success: function(data, textStatus) {
-        if (data['ok'] == true) {
-          // Done!.
-        } else {
-          alert( "Oooops!, something failed" );
-        } 
-      },
-      error: function (XMLHttpRequest, textStatus, errorThrown) {
-        alert("Ooooops!, request failed with status: " + XMLHttpRequest.status + ' ' + XMLHttpRequest.responseText);
-      }
-    });
-    return false;
-  });
-
-  $('input[type=checkbox]').click( function() {
-    $( "#" + theForm ).trigger("submit");
-    return true;
-  });
+	  $(theCheckbox).click( function() {
+	    $( "#" + theForm ).trigger("submit");
+	    return true;
+	  });
+	});
 
 });
